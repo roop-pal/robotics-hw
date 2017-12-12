79407161cc6521c80d4a51e4421f214087a6180f
Jacques Van Anh <vajacques@gmail.com>
2017-12-11 22:40:38 -0500

    comments and final collision checks

---
+++ hw5/README.md
@@ -0,0 +1,24 @@
+#Lab 4
+###Group 24 [qv2106, rmp2191, yh2886]
+
+You can find our main in rrt.py. This was written for Python2.7.
+
+Our implementation specific details are:
+* Collision detection is done by determining if a new segment-to-be-drawn intersects with any obstacle boundary. To determine an intersection, we compare the orientation of the four segment endpoints (2 endpoints of new segment + 2 endpoints of obstacle border).
+* In addition to collision with obstacle boundaries, we avoid new segments colliding with the existing tree by treating the following as a collision: the randomly generated configuration (q_rand) and its nearest neighbor (q_near) are less than step-size from each other. This is because this scenario will cause the new segment to extend from q_near past q_rand, which may bring it closer to another node in the tree (i.e. q_near ceases to be the actual nearest neighbor)
+* There is a 5% bias of choosing the goal point as the randomly generated point at each iteration. 
+* The script will terminate following 1000000 consecutive iterations of non-collision-free generated configurations.  
+
+Any special assumption we made are:
+* The program will run for at most 10000000 iterations (an invalid q_rand due to collision still counts as 1 iteration).
+* The dimensions of the world are 600x600. This is set as a global variable DIM. 
+
+Images of sample results are stored in rrt.png and rrt-bi.png for non-bidirectional and bidirectional runs respectively.
+
+The script expects command-line arguments in the following format:
+'python rtt.py <distance> <obstacles-file> <start-goal-file> <-b>'
+
+-b is an optional argument specifying the program to run bidirecitonal RRT. For example, to run the homework files with step size 20:
+'python rtt.py 20 world_obstacles.txt start_goal.txt'
+or
+'python rtt.py 20 world_obstacles.txt start_goal.txt -b'
--- hw5/rtt.py
+++ hw5/rtt.py
@@ -5,10 +5,10 @@ from matplotlib import collections as mc
 import random
 import math
 
-
 DIM = 600
-N = 1000000
+N = 10000000
 
+# Defines a node in the tree, with a reference to parent node
 class Node:
     def __init__(self, x, y, parent):
         self.xy = (x,y)
@@ -16,11 +16,16 @@ class Node:
         self.y = y
         self.parent = parent
 
+
+# Computes the Euclidean distance between 2 points
 def dist(xy1, xy2):
     x1, y1 = xy1
     x2, y2 = xy2
     return math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
 
+
+# Reads obstacle file and returns a list of segments defining the obstacle borders
+# (segments are store as lists of 2 tuples, each defining a xy-coordinate)
 def get_obstacles(obstacle_file):
     f = open(obstacle_file, 'r')
     
@@ -43,6 +48,8 @@ def get_obstacles(obstacle_file):
     f.close()
     return segments
 
+
+# Reads the start-goal file and returns the endpoints as a list of xy-coordinates
 def get_start_and_end(start_goal_file):
     f = open(start_goal_file, 'r')
 
@@ -59,6 +66,8 @@ def get_start_and_end(start_goal_file):
     return [(startx, starty), (endx, endy)]
 
 
+# Given 2 new points, draw the segment between them on the plot
+# color of the segment is determined by boolean fromStart
 def update_plot(paths, q1, q2, fromStart):
     if fromStart:
         c = 'black'
@@ -71,11 +80,12 @@ def update_plot(paths, q1, q2, fromStart):
     plt.pause(0.05)
 
 
+# Given the reached goal node, traverse back up to the root, 
+# highlighting each segment path with a yellow color
 def highlight_path(goal):
     currNode = goal
     path = []
     while currNode.parent != None:
-        #plt.scatter(currNode.x, currNode.y, color='yellow', s=[10])
         path.append([currNode.xy, currNode.parent.xy])
         currNode = currNode.parent
         
@@ -83,28 +93,11 @@ def highlight_path(goal):
     ax = plt.axes()
     ax.add_collection(lc)
 
-def build_rrt(start, goal, step, paths, obstacles):
-    root = Node(start[0], start[1], None)
-    points_list = [root]
 
-    for i in range(N):
-        if random.random() <= 0.05:
-            q_rand = goal
-        else:
-            q_rand = random.random()*DIM, random.random()*DIM
-
-        q_new = extend_rrt(points_list, q_rand, step, obstacles, paths.get_segments())
-        if q_new != None:
-            points_list.append(q_new)
-            update_plot(paths, q_new.xy, q_new.parent.xy, True)
-
-            if dist(q_new.xy, goal) <= step:
-                update_plot(paths, q_new.xy, goal, True)
-                goal_node = Node(goal[0], goal[1], q_new)
-                highlight_path(goal_node)
-                break
-
-def extend_rrt(points_list, q_rand, step, obstacles, paths):
+# Determine if rancomly generating configuration q_rand is collision-free
+# Return a new node one step size away from q_rand's nearest neighbor if collision free
+# Return None if not
+def extend_rrt(points_list, q_rand, step, obstacles):
     q_near = None
     minDist = DIM*2
     
@@ -114,9 +107,12 @@ def extend_rrt(points_list, q_rand, step, obstacles, paths):
         if d < minDist:
             minDist = d
             q_near = q
+    
+    # return q_rand is too close to tree as extending by step size might cause collision with already-drawn segments
     if dist(q_rand, q_near.xy) < step:
         return None
 
+    # find unit vector in direction q_near -> q_rand to compute q_new
     dx = q_rand[0] - q_near.x
     dy = q_rand[1] - q_near.y
     mag = math.sqrt(math.pow(dx,2) + math.pow(dy,2))
@@ -125,18 +121,61 @@ def extend_rrt(points_list, q_rand, step, obstacles, paths):
     q_newy = q_near.y + step*dy/mag
     q_new = (q_newx, q_newy)
 
-    if check_collision(q_new, q_near.xy, obstacles, paths) == False:
+    # check collision
+    if check_collision(q_new, q_near.xy, obstacles) == False:
         return Node(q_newx, q_newy, q_near)
     return None
 
+
+# Build unidirectional RRT
+def build_rrt(start, goal, step, paths, obstacles):
+    root = Node(start[0], start[1], None)
+    points_list = [root] #list of nodes
+    consecutive_none = 0 #keep track of consecutive unsuccessful iterations
+
+    for i in range(N):
+        # Bias of 5% of choosing goal node as q_rand
+        if random.random() <= 0.05:
+            q_rand = goal
+        else:
+            q_rand = random.random()*DIM, random.random()*DIM
+
+        q_new = extend_rrt(points_list, q_rand, step, obstacles)
+
+        # if new node is collision-free, extend the tree
+        if q_new != None:
+            points_list.append(q_new)
+            update_plot(paths, q_new.xy, q_new.parent.xy, True)
+
+            # check if within step size of goal
+            if dist(q_new.xy, goal) <= step:
+                if check_collision(q_new.xy, goal, obstacles) == False:
+                    update_plot(paths, q_new.xy, goal, True)
+                    goal_node = Node(goal[0], goal[1], q_new)
+                    highlight_path(goal_node)
+                    break
+            consecutive_none = 0
+
+        # Exit if too many consecutive collisions
+        else:
+            consecutive_none += 1
+            if consecutive_none > 0.1*N:
+                print "No solution found"
+                break
+
+
+# Build bidirectional RRT
 def build_bi_rrt(start, goal, step, paths_start, paths_goal, obstacles):
     root_start = Node(start[0], start[1], None)
     root_goal = Node(goal[0], goal[1], None)
-    start_points_list = [root_start]
-    goal_points_list = [root_goal]
+    start_points_list = [root_start] # list of nodes in tree rooted at start
+    goal_points_list = [root_goal] # list of nodes in tree rooted at goal
 
-    fromStart = True
+    fromStart = True # true if current iteration builds from true start node
+    consecutive_none = 0 #keep track of consecutive unsuccessful iterations
+
     for i in range(N):
+        # set appropriate goal node depending on iteration
         if fromStart:
             currGoal = goal
             goals_list = goal_points_list
@@ -148,37 +187,46 @@ def build_bi_rrt(start, goal, step, paths_start, paths_goal, obstacles):
             points_list = goal_points_list
             p = paths_goal
 
+        # Bias of 5% of choosing goal node as q_rand
         if random.random() <= 0.05:
             q_rand = currGoal
         else:
             q_rand = random.random()*DIM, random.random()*DIM
 
-        q_new = extend_rrt(points_list, q_rand, step, obstacles, p.get_segments())
+        q_new = extend_rrt(points_list, q_rand, step, obstacles)
+
+        # if new node is collision-free, extend the tree        
         if q_new != None:
             points_list.append(q_new)
             update_plot(p, q_new.xy, q_new.parent.xy, fromStart)
 
+            # check if new node is within step size of any node in opposite tree
             found_goal = False
             for g in goals_list:
                 if dist(q_new.xy, g.xy) <= step:
-                    update_plot(p, q_new.xy, g.xy, fromStart)
-                    goal_node = Node(g.x, g.y, q_new)
-                    highlight_path(goal_node)
-                    highlight_path(g)
-                    found_goal = True
-                    break
+                    if check_collision(q_new.xy, g.xy, obstacles) == False:
+                        update_plot(p, q_new.xy, g.xy, fromStart)
+                        goal_node = Node(g.x, g.y, q_new)
+                        highlight_path(goal_node)
+                        highlight_path(g)
+                        found_goal = True
+                        break
             if found_goal:
                 break
             fromStart = not fromStart
+            consecutive_none = 0
+
+        # Exit if too many consecutive collisions
+        else:
+            consecutive_none += 1
+            if consecutive_none > 0.1*N:
+                print "No solution found"
+                break
 
 
-def orientation(a, b, c):
-    if ((b[1] - a[1]) * (c[0] - b[0]) - (b[0] - a[0]) * (c[1] - b[1])) > 0:
-        return 1
-    return -1
-        
-def check_collision(q1, q2, obstacles, paths):
-    borders = obstacles #+ paths
+# Check if new segment q1-q2 would intersect with any obstacle border
+def check_collision(q1, q2, obstacles):
+    borders = obstacles
     for border in borders:
         b1 = border[0]
         b2 = border[1]
@@ -187,6 +235,14 @@ def check_collision(q1, q2, obstacles, paths):
     return False
 
 
+# Determine orientation of three points (i.e. clockwise or counterclockwise)
+def orientation(a, b, c):
+    if ((b[1] - a[1]) * (c[0] - b[0]) - (b[0] - a[0]) * (c[1] - b[1])) > 0:
+        return 1
+    return -1
+
+
+# Determine if segment a-b intersercts with segment c-d by compraing orientations
 def intersect(a, b, c, d):
     if orientation(a, c, d) == orientation(b, c, d):
         return False
@@ -196,7 +252,6 @@ def intersect(a, b, c, d):
 
 
 if __name__ == '__main__':
-    distance = 5
     if len(sys.argv) < 4 or len(sys.argv) > 5:
         print "Invalid number of arguments. Usage: python rtt.py <distance> <obstacles-file> <start-goal-file> <-b>"
         sys.exit(1)
@@ -213,7 +268,6 @@ if __name__ == '__main__':
     endpoints = get_start_and_end(sys.argv[3])
     endpoints_x, endpoints_y = zip(*endpoints)
     plt.scatter(endpoints_x, endpoints_y, color='r')
-
     obstacles_lc = mc.LineCollection(obstacles, color='b')
     paths_start = mc.LineCollection([], color='black')
     paths_goal = mc.LineCollection([], color='magenta')
@@ -226,7 +280,7 @@ if __name__ == '__main__':
     plt.ion()
     plt.gca().invert_yaxis()
 
-    # Run RTT
+    # Run RTT and save output
     if len(sys.argv) == 5:
         build_bi_rrt(endpoints[0], endpoints[1], distance, paths_start, paths_goal, obstacles)
         plt.ioff()
